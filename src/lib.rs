e46e6c93fdde0ee78aab294277c189a5bbf6b56e
Rafaeltheraven <rafael@dulfer.be>
2023-07-15 14:18:18 +0200

    Add overrides, add tests, allow regexes

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,52 @@ use proc_macro2::TokenStream as TokenStream2;
 use std::matches;
 
 use proc_macro2::{TokenTree, Spacing};
-use syn::{parse_macro_input, Result, spanned::Spanned, DeriveInput, DataEnum, Ident, Lit};
+use syn::{Result, spanned::Spanned, DeriveInput, DataEnum, Ident, Lit, LitStr};
 use quote::{ToTokens, quote};
 
-#[proc_macro_derive(Display, attributes(display_override))]
+#[proc_macro_derive(Display, attributes(display_override, display_concat))]
 pub fn logos_display(input: TokenStream) -> TokenStream {
-    let ast = parse_macro_input!(input as DeriveInput);
+    _logos_display(input.into()).into()
+}
+
+fn _logos_display(input: TokenStream2) -> TokenStream2 {
+    let ast = match syn::parse2::<DeriveInput>(input) {
+        Ok(res) => res,
+        Err(e) => return e.to_compile_error()
+    };
     let span = ast.span();
     let ident = ast.ident;
+    let mut concat = Some("/".to_string());
+    for attr in ast.attrs.into_iter() {
+        if let syn::Meta::List(l) = attr.meta {
+            if l.path.is_ident("display_concat") {
+                let as_str = l.tokens.to_string();
+                let cand = match syn::parse2::<LitStr>(l.tokens) {
+                    Ok(res) => Ok(res.value()),
+                    Err(e) => {
+                        let resp = as_str;
+                        if resp == "None" {
+                            Ok(resp)
+                        } else {
+                            Err(syn::Error::new(e.span(), "Concat must be either a string or None"))
+                        }
+                    }
+                };
+                let litstr = match cand {
+                    Ok(res) => res,
+                    Err(e) => return e.to_compile_error()
+                };
+                if litstr == "None" {
+                    concat = None;
+                } else {
+                    concat = Some(litstr);
+                }
+                break;
+            }
+        }
+    }
     let resp = match ast.data {
-        syn::Data::Enum(e) => logos_display_derive(e, &ident),
+        syn::Data::Enum(e) => logos_display_derive(e, &ident, concat),
         _ => Err(syn::Error::new(span, "Can only derive display for enums"))
     };
     match resp {
@@ -27,13 +63,13 @@ pub fn logos_display(input: TokenStream) -> TokenStream {
                         write!(f, "{}", ret)
                     }
                 }
-            }.into()
+            }
         },
-        Err(e) => e.to_compile_error().into()
+        Err(e) => e.to_compile_error()
     }
 }
 
-fn logos_display_derive(e: DataEnum, ident: &Ident) -> Result<TokenStream2> {
+fn logos_display_derive(e: DataEnum, ident: &Ident, concat: Option<String>) -> Result<TokenStream2> {
     let mut repr_map: Vec<(TokenStream2, TokenStream2)> = Vec::with_capacity(e.variants.len());
     for variant in e.variants.into_iter() {
         let args = match variant.fields {
@@ -46,9 +82,13 @@ fn logos_display_derive(e: DataEnum, ident: &Ident) -> Result<TokenStream2> {
         for attr in variant.attrs.into_iter() {
             if let syn::Meta::List(l) = attr.meta {
                 if l.path.is_ident("display_override") {
-                    found = Some(l.tokens.to_string());
+                    let litstr = match syn::parse2::<LitStr>(l.tokens) {
+                        Ok(res) => res,
+                        Err(e) => return Err(e)
+                    };
+                    found = Some(litstr.value());
                     break;
-                } else if l.path.is_ident("token") {
+                } else if l.path.is_ident("token") || l.path.is_ident("regex") {
                     let mut new_stream = TokenStream2::new();
                     let span = l.span();
                     for tt in l.tokens.into_iter() {
@@ -62,7 +102,11 @@ fn logos_display_derive(e: DataEnum, ident: &Ident) -> Result<TokenStream2> {
                     if let Lit::Str(s) = lit {
                         let string = s.value();
                         if let Some(f) = found {
-                            found = Some(format!("{}/{}", f, string))
+                            if let Some(ref conc) = concat {
+                                found = Some(format!("{}{}{}", f, conc, string))
+                            } else {
+                                found = Some(string);
+                            }
                         } else {
                             found = Some(string);
                         }
@@ -80,3 +124,163 @@ fn logos_display_derive(e: DataEnum, ident: &Ident) -> Result<TokenStream2> {
     let arms: Vec<TokenStream2> = repr_map.iter().map(|(k, v)| quote!(#ident::#k => #v,)).collect();
     Ok(quote!(#( #arms )*))
 } 
+
+#[cfg(test)]
+mod tests {
+    use super::_logos_display;
+    use proc_macro2::TokenStream;
+    use quote::quote;
+    use assert_tokenstreams_eq::assert_tokenstreams_eq;
+
+    fn expect(arms: TokenStream) -> TokenStream {
+        quote!(
+            impl std::fmt::Display for A {
+                fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                    let ret = match &self {
+                        #arms
+                    };
+                    write!(f, "{}", ret)
+                }
+            }
+        )
+    }
+
+    #[test]
+    fn test_basic() {
+        let input = quote!(
+            enum A {
+                #[token("{")]
+                LCur,
+
+                #[regex("}")]
+                RCur
+            }
+        );
+        let arms = quote!(
+            A::LCur => "{",
+            A::RCur => "}",
+        );
+        let expected = expect(arms);
+        let result = _logos_display(input);
+        assert_tokenstreams_eq!(&result, &expected);
+    }
+
+    #[test]
+    fn test_override() {
+        let input = quote!(
+            enum A {
+                #[display_override("fancy curly thing")]
+                #[token("{")]
+                LCur,
+
+                #[regex("}")]
+                RCur,
+
+                #[token("-")]
+                #[display_override("dash")]
+                Minus
+            }
+        );
+        let arms = quote!(
+            A::LCur => "fancy curly thing",
+            A::RCur => "}",
+            A::Minus => "dash",
+        );
+        let expected = expect(arms);
+        let result = _logos_display(input);
+        assert_tokenstreams_eq!(&result, &expected);
+    }
+
+    #[test]
+    fn test_quoted_string() {
+        let input = quote!(
+            enum A {
+                #[token("{")]
+                LCur,
+
+                #[regex("\".*\"")]
+                RCur
+            }
+        );
+        let arms = quote!(
+            A::LCur => "{",
+            A::RCur => "\".*\"",
+        );
+        let expected = expect(arms);
+        let result = _logos_display(input);
+        assert_tokenstreams_eq!(&result, &expected);
+    }
+
+    #[test]
+    fn test_raw_string() {
+        let input = quote!(
+            enum A {
+                #[token("{")]
+                LCur,
+
+                #[regex(r#"".*""#)]
+                RCur
+            }
+        );
+        let arms = quote!(
+            A::LCur => "{",
+            A::RCur => "\".*\""
+        );
+        let expected = expect(arms);
+        let result = _logos_display(input);
+        assert_tokenstreams_eq!(&result, &expected);
+    }
+
+    #[test]
+    fn test_concat_basic() {
+        let input = quote!(
+            enum A {
+                #[token("{")]
+                #[token("}")]
+                Cur,
+            }
+        );
+        let arms = quote!(
+            A::Cur => "{/}",
+        );
+        let expected = expect(arms);
+        let result = _logos_display(input);
+        assert_tokenstreams_eq!(&result, &expected);
+    }
+
+    #[test]
+    fn test_concat_some() {
+        let input = quote!(
+            #[display_concat(" or ")]
+            enum A {
+                #[token("{")]
+                #[token("}")]
+                Cur,
+            }
+        );
+        let arms = quote!(
+            A::Cur => "{ or }",
+        );
+        let expected = expect(arms);
+        let result = _logos_display(input);
+        assert_tokenstreams_eq!(&result, &expected);
+    }
+
+    #[test]
+    fn test_concat_none() {
+        let input = quote!(
+            #[display_concat(None)]
+            enum A {
+                #[token("{")]
+                #[token("}")]
+                Cur,
+            }
+        );
+        let arms = quote!(
+            A::Cur => "}",
+        );
+        let expected = expect(arms);
+        let result = _logos_display(input);
+        assert_tokenstreams_eq!(&result, &expected);
+    }
+}
